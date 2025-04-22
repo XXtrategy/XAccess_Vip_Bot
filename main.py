fe8da2681f2c43310fc6ab5019bfd597c46a7d34
XXtrategy <xxtrategy@gmail.com>
2025-04-22 09:46:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button))
-    app.run_webhook()
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
